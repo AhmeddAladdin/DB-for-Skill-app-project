ab045385ef560dd662408e25bb3af55578a8b9f6
Ahmed Alaa Eldin Al-Daly <154745372+AhmeddAladdin@users.noreply.github.com>
2025-02-02 01:08:35 +0200

    Update and rename DB_5_Skills_App.py to main.py

--- main.py
+++ main.py
@@ -14,14 +14,8 @@ What do you want to do?
 'q' => Quit App.
 Choose a letter: """
 
-# input option choose
-user_input = input(Input_Message).strip().lower()
-
 # User ID
 uid = input("Enter your ID: ")
-
-# space
-print("=" *50)
 
 # Import SQLite module
 import sqlite3
@@ -60,23 +54,37 @@ def Show_all_data():
     """Show all data for the assigned ID"""
     crsr.execute(f"select * from skills where user_id = {uid}")
     data = crsr.fetchall()
-
+    if len(data) == 0:
+        print("you have 0 skill")
     for key, user in enumerate(data, 1):
         print(f"{key}- name => {user[0]},", end=" ")
         print(f"progress => {user[1]}%,", end=" ")
         print(f"user_id => {user[2]}")
-
     Save_ans_Close()
 
 def Add_new_skill():
     """Adding a new skill for the assigned ID"""
     sk = input("Enter skill name: ").strip().capitalize()
-    prog = float(input("Enter your progress: ").strip())
-
-    crsr.execute(f"insert into skills(name, progress, user_id) values('{sk}', {prog}, {uid})")
-
-    print("Added successfully!")
-
+    crsr.execute(f"select name from skills where name = '{sk}' and user_id = {uid}")
+    data = crsr.fetchone()
+    if data == None:
+        prog = float(input("Enter your progress: ").strip())
+        crsr.execute(f"insert into skills(name, progress, user_id) values('{sk}', {prog}, {uid})")
+        print("Added successfully!")
+    else:
+        print("This skill is already exists, Do you want to update it?")
+        while True:
+            ask = input("choose Y for (yes) or N fo (no): ").upper()
+            if ask == "Y":
+                prog = float(input("Enter your progress: ").strip())
+                crsr.execute(f"update skills set progress = {prog} where name = '{sk}' and user_id = {uid}")
+                print("Updated successfully!")
+                break
+            elif ask == "N":
+                break
+            else:
+                print("Undefined choise, please choose Y or N")
+            continue
     Save_ans_Close()
 
 def Delete_a_skill():
@@ -94,35 +102,10 @@ def Update_skill():
     sk = input("Enter skill name: ").strip().capitalize()
     prog = float(input("Enter your progress: ").strip())
 
-#    crsr.execute(f"update skills set name = '{sk}' where user_id = {uid}")
-
-    crsr.execute(f"update skills set progress = {prog} where name = '{sk}'")
+    crsr.execute(f"update skills set progress = {prog} where name = '{sk}' and user_id = {uid}")
 
     print("Updated successfully!")
 
     Save_ans_Close()
 
-# check if choise is exists
-command_lst = ['s', 'a', 'd', 'u', 'q']
-if user_input in command_lst:
-    
-    if user_input == 's':
-        Show_all_data()
-    
-    elif user_input == 'a':
-        Add_new_skill()
-
-    elif user_input == 'd':
-        Delete_a_skill()
-
-    elif user_input == 'u':
-        Update_skill()
-
-    else:
-        Save_ans_Close()
-        print("App is Closed, Bye!") 
-
-else: 
-    print(
-    f"Error, this command \"{user_input}\" is't found,"
-      " please choose only one letter from ['s', 'a', 'd', 'u', 'q']")
+while True:
